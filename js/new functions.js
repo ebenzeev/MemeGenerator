d0afa11a1cc94b866a0a7d26fed43367d81075d5
Yakov Rakhamimov <jacobra19@gmail.com>
2018-03-22 18:17:39 +0200

    search area clears when user enters meme

--- js/new functions.js
+++ js/new functions.js
@@ -4,3 +4,22 @@ var canvas = document.getElementById("memecanvas");
 var dataURL = canvas.toDataURL();
 elLink.href = dataURL;
 }
+
+var fonts = ["Monaco","sans-serif","cursive"];
+
+function updateForCanvas(){   
+    var elCanvas = document.querySelector('.canvas-container');
+    elCanvas.style.display = '';
+    var elSearchInput = document.querySelector(".search-input");
+    elSearchInput.style.display = 'none';
+    var elFilterResult = document.querySelector(".filter-result");
+    elFilterResult.style.display = 'none';
+    var elMostCommon = document.querySelector(".most-common");
+    elMostCommon.style.display = 'none';
+    var elMemes = document.getElementById('memes');
+    elMemes.style.display = 'none';
+    var elAbout = document.getElementById('about');
+    elAbout.style.display = 'none';
+    var elContact = document.getElementById('contact');
+    elContact.style.display = 'none';
+}
